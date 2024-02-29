30808fb3cae328a83ef5f040cff873a6d40a38c0
Josh Broughton <josh.broughton47@gmail.com>
2024-02-28 18:54:21 -0700

    add initial framer motion setup

--- react-client/src/components/NasaImage.js
+++ react-client/src/components/NasaImage.js
@@ -1,20 +1,30 @@
+import { motion } from "framer-motion";
+
 import { useGetApodQuery } from "../slices/nasaApi";
 
-export default function NasaImage({ imageUrl, imageAlt }) {
+export default function NasaImage() {
   const { data, isError, isLoading, isSuccess, error } = useGetApodQuery();
 
   if (isLoading) {
     return(<p>Image is loading...</p>)
   } else if (isSuccess) {
     return(
-      <div className="image-container">
+      <motion.div 
+        className="image-container"
+        initial={{ opacity: 0, scale: 1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 1,
+          delay: 1,
+        }}
+        >
         <h2>{data.title}</h2>
         <a className="image-anchor" href={data.hdurl}>
           <img className="full-image" src={data.hdurl} alt={data.explanation}/>
         </a>
         <p className="explanation">{data.explanation}</p>
         <p className="explanation">Image copyright: {data.copyright}</p>
-      </div>
+      </motion.div>
     )
   }
 }
