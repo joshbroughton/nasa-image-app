cb88bbd3adcae74bf380f08efd6088747af39554
= <=>
2024-02-28 22:01:30 -0700

    added exit animation to loading icon

--- react-client/src/components/Image.js
+++ react-client/src/components/Image.js
@@ -1,23 +1,45 @@
-import { useParams, useLocation } from "react-router-dom"
+import { useParams, useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import { orbit } from "ldrs";
+
 import { useGetImageQuery } from "../slices/nasaApi";
 
 export default function Details() {
+  orbit.register();
+
   const { nasaID } = useParams();
   const { state } = useLocation();
-  const { data, isError, isLoading, isSuccess, error } = useGetImageQuery({nasaID: nasaID});
+  const { data, isLoading, isSuccess } = useGetImageQuery({nasaID: nasaID});
 
-  console.log(state);
-  if (isLoading) {
-    return(<p>Loading...</p>)
-  } else if (isSuccess) {
-    console.log(data);
-    return(
-      <div className="image-container">
+  return(
+    <AnimatePresence mode="wait">
+        {isLoading && <motion.div
+          key="loader"
+          className="image-container"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 1 }}
+          exit={{y: -300,
+            opacity: 0,
+            transition: {
+              duration: 1,
+              ease: "easeInOut",}}}
+          >
+          <l-orbit size="100" color="white"></l-orbit>
+        </motion.div>}
+      {isSuccess && <motion.div
+        key="image"
+        className="image-container"
+        initial={{ opacity: 0, scale: 1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 3,
+        }}
+        >
         <h2>{state.title}</h2>
         <a className="image-anchor" href={data.collection.items[0].href}>
-          <img className="full-image" src={data.collection.items[0].href} alt="space"/>
+          <img className="full-image" src={data.collection.items[1].href} alt="space" />
         </a>
-      </div>
-    )
-  }
+      </motion.div>}
+    </AnimatePresence>
+  )
 }
--- react-client/src/components/NasaImage.js
+++ react-client/src/components/NasaImage.js
@@ -7,39 +7,37 @@ export default function NasaImage() {
   orbit.register();
   const { data, isLoading, isSuccess } = useGetApodQuery();
 
-
-  if (isLoading) {
     return(
-      <AnimatePresence>
-        {isLoading && <motion.div
+      <AnimatePresence mode="wait">
+          {isLoading && <motion.div
+            key="loader"
+            className="image-container"
+            initial={{ opacity: 1 }}
+            animate={{ opacity: 1 }}
+            exit={{y: -300,
+              opacity: 0,
+              transition: {
+                duration: 1,
+                ease: "easeInOut",}}}
+            >
+            <l-orbit size="100" color="white"></l-orbit>
+          </motion.div>}
+        {isSuccess && <motion.div
+          key="image"
           className="image-container"
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0, scale: 0.5 }}
-          transition={{ duration: 1, delay: 3 }}
+          initial={{ opacity: 0, scale: 1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 1,
+          }}
           >
-          <l-orbit size="100" color="white"></l-orbit>
+          <h2>{data.title}</h2>
+          <a className="image-anchor" href={data.hdurl}>
+            <img className="full-image" src={data.hdurl} alt={data.explanation}/>
+          </a>
+          <p className="explanation">{data.explanation}</p>
+          <p className="explanation">Image copyright: {data.copyright}</p>
         </motion.div>}
       </AnimatePresence>
-      )
-  } else if (isSuccess) {
-    return(
-      <motion.div
-        className="image-container"
-        initial={{ opacity: 0, scale: 1 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 1,
-          delay: 1,
-        }}
-        >
-        <h2>{data.title}</h2>
-        <a className="image-anchor" href={data.hdurl}>
-          <img className="full-image" src={data.hdurl} alt={data.explanation}/>
-        </a>
-        <p className="explanation">{data.explanation}</p>
-        <p className="explanation">Image copyright: {data.copyright}</p>
-      </motion.div>
     )
-  }
 }
